85cce7f8017a185dbb4cbdc96930e5a76b5fc95f
AdityaEthape <aditya.ethape@saint-gobain.com>
2024-12-04 11:32:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
+ st.dataframe(pd_df)
+ st.stop()
 
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:'
@@ -43,8 +43,8 @@ if ingredients_list:
 
        
        st.subheader(fruit_chosen + ' Nutrition information')
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-      # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+      # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)   
        # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)   
 
